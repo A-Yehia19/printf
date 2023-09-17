6aef437690a68a8922c8cc28cdf310f699a49de3
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-17 15:38:58 +0300

    added test folder for main

--- test/main.c
+++ test/main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.h"
+#include "../main.h"
 
 /**
  * main - Entry point
@@ -13,9 +13,9 @@ int main(void)
 	int len;
 	int len2;
 
-	len2 = printf("%%%d\n", 1111111111);
+	len2 = printf("%s - %c\n", "Hello", 'H');
 	printf("-------------------------------------------------\n");
-	len = _printf("%%%d\n",1111111111);
+	len = _printf("%s - %c\n", "Hello", 'H');
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	return (0);
