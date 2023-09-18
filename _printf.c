2eeb4131ac16958ca3d8d40cad16cbfa7169b1af
A-Yehia19 <abdullah.yehia19@gmail.com>
2023-09-18 16:07:17 +0300

    testing case

--- _printf.c
+++ _printf.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == '\0')
-				return (len);
+				return (-1);
 
 			else if (format[i] == 'c')
 				len += _putchar(va_arg(args, int));
